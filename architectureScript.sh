83430301c923a65f6ead2fb8c7e6033891ed6021
rodrigo <asdf@gmail.com>
2016-06-17 09:51:49 +0200

    route changed to /var/www/html/practicaHost

--- architectureScript.sh
+++ architectureScript.sh
@@ -1,49 +1,75 @@
 #!/bin/bash
 
-
 function pageContent {
-	echo "	
-		<div>
-			Esta es la pagina $COUNTER
-		</div>
-		<input type='button' value=' Anterior ' onclick='goHist(-1)' />
-		<input type='button' value=' Siguiente ' onclick='goHist(1)' />
-	"
+
+	case $COUNTER in
+     1)
+        echo "	
+			<div>
+				This is the site number $COUNTER
+			</div> 
+			<a href='$(($COUNTER+1)).html'> Next </a>
+		"
+     ;;
+     40)
+        echo "	
+			<div>
+				This is the site number $COUNTER
+			</div>
+			<a href='$(($COUNTER-1)).html'> Previous </a>
+		"
+     ;;
+     *)
+        echo "	
+			<div>
+				This is the site number $COUNTER
+			</div>
+			<a href='$(($COUNTER-1)).html'> Previous </a> 
+			<a href='$(($COUNTER+1)).html'> Next </a>
+		"
+     ;;
+  esac
 }
+
 function createSites {
 	while [  $COUNTER -lt 41 ]; do
-   		pageContent > /var/www/html/"$COUNTER".html    	
+		mkdir /var/www/html/practicaHost 2> /dev/null
+   		pageContent > /var/www/html/practicaHost/"$COUNTER".html    	
     	let COUNTER=COUNTER+1 
 	done
 }
 
+function createVirtualHost {
+	echo "
+		<VirtualHost *:80>
+			DocumentRoot 'var/www/html/practicaHost'
+ 			ServerName app.localhost
+ 			ProxyRequests Off
+
+ 			<Proxy *>
+ 				Order deny, allow
+ 				Allow from all
+ 			</Proxy>
+	 		<Location />
+	 			ProxyPass http://localhost:58080/
+	 			ProxyPassReverse http://localhost:58080/
+	 		</Location>
+	 	</VirtualHost>
+	" > /etc/apache2/sites-available/practicaHost.conf
+}
+
 #Variables declaration
-COUNTER=0
+COUNTER=1
 
 
-#Install Apache
-#sudo apt-get install apache2
-
+# #Install apache2
+# echo Installing Apache2 and configuring it...
+# sudo apt-get install apache2
+# sudo chown -R $USER:$USER /var/www
+# cat /etc/apache2/envvars | tr 'www-data' '$USER'
 
 #Crear VirtualHost
-
+createVirtualHost
 
 #Crear sites
 createSites
-
-
-
-# <VirtualHost *:80>
-# 	ServerName app.localhost
-# 	ProxyRequests Off
-	
-# 	<Proxy *>
-# 		Order deny, allow
-# 		Allow from all
-# 	</Proxy>
-
-# 	<Location />
-# 		ProxyPass http://localhost:58080/
-# 		ProxyPassReverse http://localhost:58080/
-# 	</Location>
-# </VirtualHost>
